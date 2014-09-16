30429748a6eebd60d44d7f829a530237e1534087
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2014-09-17 00:33:52 +0100

    add function to create mask based on region of interest

---
+++ R/create_roimask.R
@@ -0,0 +1,29 @@
+#' Build Mask Raster used to crop images to ROI area -------------------------------
+#'# Mask file must be created. It is a mandatory step of the process
+#'# considering the way it was setup
+#'## 1st: Read a single Band to get the desired extent based on satellite images
+#'## Change function arguments for the ROI and polygon to apply mask
+f.CreateRoiMask <- function(x = x, roi = roi2, maskpoly = ae2){
+  x <- grep(".tif$", list.files(file.path(dir.work, dir.fun), all.files = F),
+            ignore.case = TRUE, value = TRUE)[1] 
+  i.band <- raster(file.path(dir.work, dir.fun, x),
+                   package = "raster")
+  ##dataType(band) # Must be INT2U for Landsat 8. Range of Values: 0 to 65534
+  stopifnot(!is.na(i.band@crs)) # Check projection
+  ## Create Extent object from ae shapefile
+  if(is.null(roi)){
+    i.roi <- extent(ae2)
+  } else i.roi <- extent(roi)
+  # Crop Landsat Scene to AE extent
+  i.bandae <- crop(i.band, i.roi) # Crop band to AE Extent
+  ## 2nd: Create the Mask raster to the croped band extent
+  ae.r <- i.bandae # Raster AE: resolucao 30m (Landast)
+  ae.r[] <- 1 # Defalt value
+  ## Overlay AE poly to AE Extent raster
+  ## Mask will have 1 and NA values
+  msk.ae <- mask(ae.r, maskpoly, updatevalue=NA)
+  #dataType(mask_ae) <- "INT1U" 
+  ## Evaluate rasters
+  stopifnot(compareRaster(msk.ae, i.bandae)) 
+  msk.ae
+}
